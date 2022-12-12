50cb3cbcfd81cb422c70c560c5948f98025e68a3
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-12-12 13:37:47 -0500

    withCredials

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -3,9 +3,10 @@ import axios from "axios";
 const Axios = axios.create({
   baseURL:
     process.env.REACT_APP_AXIOS === "development"
-      ? "http://localhost:3020"
+      ? "http://localhost:3020/api"
       : "/api",
   timeout: 50000,
+  withCredentials: true,
 });
 
 export default Axios;
